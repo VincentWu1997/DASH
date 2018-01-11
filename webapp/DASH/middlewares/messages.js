c90afffa2e193922c2034b3dad40888ddcd1198d
benjipatrick <benjipatrick@hotmail.co.uk>
2018-01-11 14:36:04 +0000

    messaging things

--- webapp/DASH/middlewares/messages.js
+++ webapp/DASH/middlewares/messages.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var router = express.Router()
 
-router.get('/', function(req, res) {
-    console.log("/messages")
+// inputs email, password, staffIdentifier(optional)  outputs all messages associated with said account
+router.get('/get_messages', function(req, res) {
+    console.log("/messages/get_messages")
+})
+
+// inputs email, password, message, staffID function saves the messages in the database with 
+router.post('/send_message', function(req, res) {
+    console.log("/messages/send_message")
 })
 
 module.exports = router
