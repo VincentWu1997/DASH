c8aa85f78292677d424c0d68c9a90e736cd492de
benjipatrick <benjipatrick@hotmail.co.uk>
2018-01-11 06:29:13 +0000

    Messing around with database stuff

--- webapp/DASH/controllers/index.js
+++ webapp/DASH/controllers/index.js
@@ -19,6 +19,41 @@ router.use('/feedback', feedback)
 router.use('/news', news)
 router.use('/logon', logon)
 
+// setting up database
+const sqlite3 = require('sqlite3').verbose()
+let db = new sqlite3.Database(path.join(__dirname, '..', 'models/data/database.db'), (err) => {
+    if(err) {
+        console.log(err.message)
+    } else {
+        console.log('Connected to the database.')
+    }
+})
+db.serialize(function() {
+    // creating tables
+    db.run('CREATE TABLE IF NOT EXISTS Staff_Account (staffID INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR NOT NULL, password VARCHAR not null, canEditNews BOOLEAN DEFAULT FALSE)')
+    db.run('CREATE TABLE IF NOT EXISTS User_Account (userID INTEGER PRIMARY KEY AUTOINCREMENT, email VARCHAR NOT NULL, password VARCHAR not null, staffID INTEGER NOT NULL, FOREIGN KEY (staffID) REFERENCES Staff_Account (staffID))')
+    db.run('CREATE TABLE IF NOT EXISTS News (newsID INTEGER PRIMARY KEY AUTOINCREMENT, staffID INTEGER NOT NULL, title VARCHAR not null, date DATETIME DEFAULT CURRENT_TIMESTAMP, blurb VARCHAR,  FOREIGN KEY(staffID) REFERENCES Staff_Account(staffID))')
+    db.run('CREATE TABLE IF NOT EXISTS Feedback (feedbackID INTEGER PRIMARY KEY AUTOINCREMENT, userID INTEGER NOT NULL, feedback VARCHAR not null, date DATETIME DEFAULT CURRENT_TIMESTAMP, FOREIGN KEY(userID) REFERENCES User_Account(userID))')    
+    db.run('CREATE TABLE IF NOT EXISTS Referral_Form (referralID INTEGER PRIMARY KEY AUTOINCREMENT, userID INTEGER NOT NULL, referralJSON VARCHAR not null, date DATETIME DEFAULT CURRENT_TIMESTAMP, status INTEGER DEFAULT 0, FOREIGN KEY(userID) REFERENCES User_Account(userID))')
+    
+    // inserting test data
+    db.run('INSERT INTO Staff_Account(staffID, username, password, canEditNews) VALUES(0, "staff", "password", 0)', (err) => {
+        if(err) console.log(err)
+        else console.log('Insert success')
+    })
+    
+    db.all('SELECT * FROM Staff_Account', [], (err, rows) => {
+        if (err) {
+          throw err;
+        }
+        rows.forEach((row) => {
+          console.log(row);
+        });
+      });
+
+    db.close()
+})
+
 // testing routes....
 router.use(express.static(path.join(__dirname, '..', 'views')))
 router.get('/', function(req, res) {
